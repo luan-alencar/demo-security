ccd8626a7ee71061ec1755b2b5725221a23bb23b
luan-alencar <luanalencar134@gmail.com>
2020-10-30 16:27:12 -0300

    realizando manutencao na classe

--- src/main/java/david/augusto/luan/config/SecurityConfig.java
+++ src/main/java/david/augusto/luan/config/SecurityConfig.java
@@ -9,14 +9,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 		// atraves desse metodo a gnt inicia as configurações referente 
-		// ao que vai ter acesso ou nao na nossa aplicacao
-		http.authorizeRequests()
-		.antMatchers("/")
-		.permitAll() // este metodo vai tornar public essas uri q eu colocar seguidas por ele
-		.anyRequest()
-		.authenticated();
+				// ao que vai ter acesso ou nao na nossa aplicacao
+				http.authorizeRequests()
+				.antMatchers("/webjars/**", "/css/**", "/image/**","/js/**").permitAll()
+				.antMatchers("/", "/home")
+				.permitAll() // este metodo vai tornar public essas uri q eu colocar seguidas por ele
+				.anyRequest().authenticated();
+		
 	}
 
 }
