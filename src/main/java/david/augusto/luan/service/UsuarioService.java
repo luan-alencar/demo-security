9d3f9943126388301fb1c1949427f5f3e4ab0f9b
luan-alencar <luanalencar134@gmail.com>
2020-10-31 09:19:19 -0300

    bug encontrado e corrigido em getAuthorities(), bug estava no for()

--- src/main/java/david/augusto/luan/service/UsuarioService.java
+++ src/main/java/david/augusto/luan/service/UsuarioService.java
@@ -16,31 +16,33 @@ import david.augusto.luan.domain.Usuario;
 import david.augusto.luan.repository.UsuarioRepository;
 
 @Service
-public class UsuarioService implements UserDetailsService{
+public class UsuarioService implements UserDetailsService {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	// testa se o email ja esta cadastrado
-	@Transactional(readOnly = true) 
+	@Transactional(readOnly = true)
 	public Usuario buscarPorEmail(String email) {
 		return usuarioRepository.findByEmail(email);
 	}
 
-	@Override @Transactional(readOnly = true) // tudo que tiver dentro desse metodo vai ser gerenciado por uma transacao
-	// recebe uma String como argumento e ela vai ser o email q o usuario digitou no formulario	
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	@Override
+	@Transactional(readOnly = true) // tudo que tiver dentro desse metodo vai ser gerenciado por uma transacao
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {// recebe uma String como
+																								// argumento e ela vai
+																								// ser o email q o
+																								// usuario digitou no
+																								// formulario
 		Usuario usuario = buscarPorEmail(username);
-		// User e uma classe do spring que implementa UserDetaisl	
-		return new User(
-				usuario.getEmail(),
-				usuario.getSenha(),
+		// User e uma classe do spring que implementa UserDetaisl
+		return new User(usuario.getEmail(), usuario.getSenha(),
 				AuthorityUtils.createAuthorityList(getAuthorities(usuario.getPerfis())));
 	}
-	
+
 	private String[] getAuthorities(List<Perfil> perfis) {
 		String[] authorities = new String[perfis.size()];
-		for (int i = 0; i <= perfis.size(); i++) {
+		for (int i = 0; i < perfis.size(); i++) {
 			authorities[i] = perfis.get(i).getDesc();
 		}
 		return authorities;
