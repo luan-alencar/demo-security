a03f1bf126b3589f241dd209bd05b0629a515ed8
luan-alencar <luanalencar134@gmail.com>
2020-10-31 09:00:55 -0300

    testando BCryptPasswordEncoder

--- src/main/java/david/augusto/luan/DemoSecurityApplication.java
+++ src/main/java/david/augusto/luan/DemoSecurityApplication.java
@@ -2,16 +2,13 @@ package david.augusto.luan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("luan"));
-		System.out.println(new BCryptPasswordEncoder().encode("root"));
-		System.out.println(new BCryptPasswordEncoder().encode("root"));
-//		SpringApplication.run(DemoSecurityApplication.class, args);
+//		System.out.println(new BCryptPasswordEncoder().encode("1234"));
+		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 
 }
