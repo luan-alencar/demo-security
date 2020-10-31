dcb79faf6b3be2114c0215e7e3335c9d64d033fb
luan-alencar <luanalencar134@gmail.com>
2020-10-31 10:34:02 -0300

    adicionando classe UsuarioController

---
+++ src/main/java/david/augusto/luan/web/controller/UsuarioController.java
@@ -0,0 +1,19 @@
+package david.augusto.luan.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import david.augusto.luan.domain.Usuario;
+
+@Controller
+@RequestMapping
+public class UsuarioController {
+
+	// abrir cadastro de usuarios (medicos/admin/pacientes/)
+	@GetMapping("/novo/cadastro/usuario")
+	public String cadastroPorAdminParaAdminMedicoAdminPaciente(Usuario usuario) {
+	
+		return "usuario/cadastro"; 
+	}
+}
