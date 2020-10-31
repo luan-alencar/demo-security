243ee66dbf04c40b87df4c7887970fe628ab9022
luan-alencar <luanalencar134@gmail.com>
2020-10-31 11:42:17 -0300

    adicionando metodo acessoNegado tendo como parametro um HttpServletResponse

--- src/main/java/david/augusto/luan/web/controller/HomeController.java
+++ src/main/java/david/augusto/luan/web/controller/HomeController.java
@@ -1,5 +1,7 @@
 package david.augusto.luan.web.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,26 +10,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping({"/", "/home"})
+	@GetMapping({ "/", "/home" })
 	public String home() {
 		return "home";
-	}	
-	
+	}
 
 	// abrir pagina login
-	@GetMapping({"/login"})
+	@GetMapping({ "/login" })
 	public String login() {
 		return "login";
-	}	
-	
+	}
 
 	// login invalido
-	@GetMapping({"/login-error"})
+	@GetMapping({ "/login-error" })
 	public String loginError(ModelMap model) {
 		model.addAttribute("alerta", "erro");
 		model.addAttribute("titulo", "Credenciais invalidas!");
 		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
 		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros ja ativados.");
 		return "login";
-	}	
+	}
+
+	// acesso negado
+	@GetMapping({ "/acesso-negado" })
+	public String acessoNegado(ModelMap model, HttpServletResponse resp) { // com o resp eu posso pegar os tatus da
+																			// requisicao
+		model.addAttribute("alerta", resp.getStatus()); // vai retornar o cod do status
+		model.addAttribute("error", "Acesso negado!");
+		model.addAttribute("message", "Você nao tem permissão para acesso a esta área ou ação.");
+		return "error";
+	}
 }
