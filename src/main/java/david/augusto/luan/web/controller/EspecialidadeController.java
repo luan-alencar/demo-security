c8027d5a64a9098647972fa4c53fea5bc6648583
luan-alencar <luanalencar134@gmail.com>
2020-10-31 12:04:23 -0300

    desenvolvimento de classe que serve como Controller a Especialidade

---
+++ src/main/java/david/augusto/luan/web/controller/EspecialidadeController.java
@@ -0,0 +1,32 @@
+package david.augusto.luan.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import david.augusto.luan.domain.Especialidade;
+import david.augusto.luan.service.EspecialidadeService;
+
+@Controller
+@RequestMapping("especialidades")
+public class EspecialidadeController {
+
+	@Autowired
+	private EspecialidadeService service;
+
+	@GetMapping
+	public String abrirEspecialidades(Especialidade especialidade) {
+		
+		return "especialidade/especialidade";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Especialidade especialdidade, RedirectAttributes attr) {
+		service.salvar(especialdidade);
+		attr.addFlashAttribute("sucesso", "Operação realizada com sucesso");
+		return "redirect:/especialidades";
+	}
+}
