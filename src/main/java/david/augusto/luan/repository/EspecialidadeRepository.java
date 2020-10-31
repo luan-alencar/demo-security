9724efcd1e59656153ba50c87908b981a7eb9f92
luan-alencar <luanalencar134@gmail.com>
2020-10-31 12:03:05 -0300

    criacao de uma de um repository para Especialidade

---
+++ src/main/java/david/augusto/luan/repository/EspecialidadeRepository.java
@@ -0,0 +1,10 @@
+package david.augusto.luan.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import david.augusto.luan.domain.Especialidade;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>{
+
+}
