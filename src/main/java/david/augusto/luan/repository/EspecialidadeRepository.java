18be0a300a067b7f2dc721fd7fff5a5a4df5bd97
luan-alencar <luanalencar134@gmail.com>
2020-11-01 17:18:37 -0300

    manutencao de classe

--- src/main/java/david/augusto/luan/repository/EspecialidadeRepository.java
+++ src/main/java/david/augusto/luan/repository/EspecialidadeRepository.java
@@ -1,10 +1,16 @@
 package david.augusto.luan.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import david.augusto.luan.domain.Especialidade;
 
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>{
+
+	@Query("select e from Especialidade e where e.titulo like :search%")
+	Page<Especialidade> findAllByTitulo(String search, Pageable pageable);
 
 }
