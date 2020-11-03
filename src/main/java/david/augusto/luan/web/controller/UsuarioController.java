5bba866c51af21910fcabc504aa944859d4b98ae
luan-alencar <luanalencar134@gmail.com>
2020-11-03 08:58:20 -0300

    add comentario e manutencao no método contanis, pois, o correto seria containsAll

--- src/main/java/david/augusto/luan/web/controller/UsuarioController.java
+++ src/main/java/david/augusto/luan/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,13 @@ import david.augusto.luan.service.UsuarioService;
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
+
+	/*
+	 * Uma excessão de status 500 iNTERNAL SERVER ERROR envia uma requisição, essa
+	 * requisição é aceita pela aplicação, então a requisição chega nos Controllers
+	 * e entra nos Controllers e assim cada chamada de m[etodo lá dentro vai
+	 * chamando cada parte da aplicação então a requisição.
+	 */
 
 	@Autowired
 	private UsuarioService service;
@@ -44,20 +52,25 @@ public class UsuarioController {
 	}
 
 	// salvar o cadastro de usuarios por administrador
-	@SuppressWarnings("unlikely-arg-type")
 	@PostMapping("/cadastro/salvar")
 	public String salvarUsuarios(Usuario usuario, RedirectAttributes redirect) {
 		List<Perfil> perfis = usuario.getPerfis();
 		// se for maior q dois ja sabe que esta cadastrando 3 perfis e nao pode
-		if (perfis.size() > 2 || perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L)))
-				|| perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) { 
+		if (perfis.size() > 2 || perfis.containsAll(Arrays.asList(new Perfil(1L), new Perfil(3L)))
+				|| perfis.containsAll(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
 			redirect.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico");
 			redirect.addFlashAttribute("usuario", usuario);
-			
+
 		} else {
-			service.salvarUsuario(usuario);
-			redirect.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+			try {
+				service.salvarUsuario(usuario);
+				redirect.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+			} catch (DataAccessException e) {
+				redirect.addFlashAttribute("falha",
+						"Operação não pode ser realizada! Paciente não pode ser Admin e/ou Médico");
+			}
+
 		}
-		return "redirect:/u/novo/cadastro/usuario"; // vai direcionar ao primeiro metodo	
+		return "redirect:/u/novo/cadastro/usuario"; // vai direcionar ao primeiro metodo
 	}
 }
