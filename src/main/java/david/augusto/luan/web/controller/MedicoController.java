4326b11fd4014788dcecd0dabdbcce66fe7db4e2
luan-alencar <luanalencar134@gmail.com>
2020-11-08 20:10:13 -0300

    adicionando metodos de salvar e editar medico

--- src/main/java/david/augusto/luan/web/controller/MedicoController.java
+++ src/main/java/david/augusto/luan/web/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import david.augusto.luan.domain.Medico;
 
@@ -17,4 +18,19 @@ public class MedicoController {
 		
 			return "medico/cadastro"; 
 		}
+		
+		// salvar medico
+		@GetMapping({"/salvar"})
+		public String salvar(Medico medico, RedirectAttributes attr) {
+		
+			return "medico/cadastro"; 
+		}
+		
+		// editar medico
+		@GetMapping({"/editar"})
+		public String editar(Medico medico, RedirectAttributes attr) {
+		
+			return "medico/cadastro"; 
+		}
+
 }
